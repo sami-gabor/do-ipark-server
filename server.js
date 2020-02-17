94e2f77a3e711d185b734b4c7c1e5cf3cf1c6013
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-02-17 12:13:49 +0200

    basic test auth

--- server.js
+++ server.js
@@ -1,91 +1,46 @@
 const express = require('express');
-const basicAuth = require('express-basic-auth');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
-
-const users = {
-    admin: '123',
-    user: '456',
-}
-
-const auth = basicAuth({ users });
-
 const PORT = process.env.PORT || 5000;
 
-app.use(cookieParser('82e4e438a0705fabf61f9854e3b575af'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '/client/build')));
-
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
-
-app.get('/authenticate', auth, (req, res) => {
-  const { user } = req.auth;
-  console.log(1, 'authenticate: ', req.auth);
-
-  if (user === 'admin') {
-    res.cookie('name', 'admin').send({ screen: 'admin' });
-  } else if (user === 'user') {
-    res.cookie('name', 'user').send({ screen: 'user' });
-  }
-
-  // const options = {
-  //   httpOnly: true,
-  //   signed: true,
-  // };
-  // if (req.auth.user === 'admin') {
-  //   res.cookie('name', 'admin', options).send({ screen: 'admin' });
-  // } else if (req.auth.user === 'user') {
-  //   res.cookie('name', 'user', options).send({ screen: 'user' });
-  // }
-});
-
 app.get('/read-cookie', (req, res) => {
-  const { name } = req.cookies;
+  console.log(req.cookies.name);
   
-  if(name === 'admin') {
-    console.log(11, 'read-cookie', name);
-    res.send({ screen: 'admin' });
-  } else if (name === 'user') {
-    console.log(12, 'read-cookie', name);
-    res.send({ screen: 'user' });
+  if (req.cookies.name === 'admin') {
+    res.json({ screen: 'admin' });
   } else {
-    console.log(13, 'read-cookie', name);
-    res.send({ screen: 'auth' });
+    res.json({ screen: 'authPage' });
   }
-//   if (req.signedCookies.name === 'admin') {
-//     res.send({ screen: 'admin' });
-//   } else if (req.signedCookies.name === 'user') {
-//     res.send({ screen: 'user' });
-//   } else {
-//     res.send({ screen: 'auth' });
-//   }
 });
 
 app.get('/clear-cookie', (req, res) => {
-  console.log(20, 'clear-cookie', req.cookies.name);
   res.clearCookie('name').end();
-});
+})
 
-// app.get('/get-data', (req, res) => {
-//   if (req.signedCookies.name === 'admin') {
-//     res.send('This is admin panel');
-//   } else if (req.signedCookies.name === 'user') {
-//     res.send('This is user data');
-//   } else {
-//     res.end();
-//   }
-// });
+app.post('/authenticate', (req, res) => {
+  const { email, password } = req.body;
+  
+  if (email === 'admin' && password === '123') {
+    res.cookie('name', 'admin');
+    res.send({ screen: 'admin' });
+  } else {
+    res.send({ screen: 'authPage' });
+  }
+})
 
-
-// app.get('/data', (req, res) => {
-//     res.send(users);
-// });
+app.get('/parking-spots', (req, res) => {
+  res.json({ list: [1, 3, 5] });
+})
